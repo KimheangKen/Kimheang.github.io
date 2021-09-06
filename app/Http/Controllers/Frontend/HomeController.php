9f6e69b38389a7b38e3fa7ed289e2855b43bfc8d
kimheang123 <kimheangken68@gmail.com>
2021-09-06 20:03:13 +0700

    hosting

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Frontend\FrontendController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends FrontendController
+{
+    public function index()
+    {
+        return view('frontend.home');
+    }
+}
